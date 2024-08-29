22f0367f98f9c5fa88fae193fedf82e2fa2a2246
ymabrouk <yassermabrouk@Yassers-MacBook-Pro.local>
2024-08-29 15:46:22 +0300

    Initial version

---
+++ src/test/java/com/jeroenreijn/demomicrometerprometheusgrafana/DemoMicrometerPrometheusGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeroenreijn.demomicrometerprometheusgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoMicrometerPrometheusGrafanaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
